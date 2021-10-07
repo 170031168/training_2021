dd9fd7de2d0804e1fce39c49f11a313833580b51
170031168 <170031168@kluniversity.in>
2021-10-07 15:03:38 +0530

    REQ-101 - BookService 2 is builded as required

---
+++ book-service-2/src/test/java/com/lumen/demo/BookService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
