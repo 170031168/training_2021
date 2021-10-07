07c85609a7f0c29151a44d0e7afc01b814546370
170031168 <170031168@kluniversity.in>
2021-10-07 12:07:21 +0530

    REQ-102 Added the model class Book with required fields

---
+++ book-service/src/main/java/com/lumen/demo/pojo/Book.java
@@ -0,0 +1,17 @@
+package com.lumen.demo.pojo;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Book {
+	int bookId;
+	String bookName;
+	String author;
+}
