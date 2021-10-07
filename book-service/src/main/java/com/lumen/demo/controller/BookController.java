af1ef1a26a0ad48a212e358644cec395129f0d46
170031168 <170031168@kluniversity.in>
2021-10-07 12:26:55 +0530

    REQ-105 - Updated the RestController Class with some Mappings

--- book-service/src/main/java/com/lumen/demo/controller/BookController.java
+++ book-service/src/main/java/com/lumen/demo/controller/BookController.java
@@ -3,12 +3,14 @@ package com.lumen.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lumen.demo.pojo.Book;
 import com.lumen.demo.service.BookService;
 
 @RestController
+@RequestMapping("/api/v1")
 public class BookController {
 	
 	@Autowired
